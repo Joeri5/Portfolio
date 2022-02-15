a906bda9df171b2084a9a68b7b1e334200fcdda7
joeri5 <joerischenk@outlook.com>
2022-02-15 19:57:36 +0100

    started working on the hamburger menu

---
+++ main.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("js-button");
+const nav = document.getElementById("js-hamburger");
+const body = document.getElementById("js-body");
+const hero = document.getElementById("js-main-hero");
+
+let scroller = false;
+
+button.onclick = function () {
+  nav.style.visibility = "visible";
+  nav.style.opacity = "1";
+  hero.style.visibility = "hidden";
+};
+
+body.window.posi;
+
+// navclose.onclick = function () {
+//   nav.style.visibility = "hidden";
+//   nav.style.opacity = "0";
+//   body.style.overflow = "hidden";
+// };
